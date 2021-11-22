d090e0cf5e4e022f3122a2f71569ff0ba0072be5
RalfiWardhana <ralfigeophysics@gmail.com>
2021-11-22 12:23:32 +0700

    ubah env port

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors")
 const app = express();
 const route = require("./src/routes/index")
-const port = 2010;
+// const port = 2010;
+const port = process.env.PORT || 2011;
 
 app.use(cors())
 app.use(express.json())
 app.use("/App/v1",route);
 app.use("/uploads",express.static("uploads"))
 
+app.get('/', function (req, res) {
+  res.status(200).json({ message: 'Ya ini udah bisa diakses' });
+});
 app.listen(port,()=>{
   console.log(`Listening ${port} is successfully`)
 })
